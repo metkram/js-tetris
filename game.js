2ba980c461e3c7736e91c38daea445d2e4c6c9b3
unknown <metkraming@gmail.com>
2020-06-12 15:28:08 +0300

    add basic concatination functionality of droping figure and matrix

--- game.js
+++ game.js
@@ -6,17 +6,36 @@ class Game {
     this.matrix = this.newMatrix();
     this.shape = new IShape();
     this.renderField();
-    setInterval(() => this.step(), 1000);
-    setTimeout(() => this.shape.move("right"), 7000);
-    setTimeout(() => this.shape.move("left"), 9000);
-    setTimeout(() => this.shape.move("turn"), 11000);
+    setInterval(() => this.step(), 500);
+    setTimeout(() => this.shape.move("right"), 1000);
+    setTimeout(() => this.shape.move("right"), 2000);
+    setTimeout(() => this.shape.move("right"), 3000);
+    setTimeout(() => this.shape.move("left"), 11000);
+    setTimeout(() => this.shape.move("left"), 12000);
+    setTimeout(() => this.shape.move("left"), 13000);
   }
   step() {
     // this.nextFigure();
-    this.shape.bottomCoordinate++;
-    console.log(this.shape.bottomCoordinate);
-    if (this.shape.bottomCoordinate == 24) this.shape.bottomCoordinate = 4;
     this.renderField();
+    console.log(this.shape.bottomCoordinate);
+    for (let i = 3; i > 1; i--) {
+      for (let u = 0; u < 10; u++) {
+        if (this.shape.matrix[i][u] == 1 && this.shape.matrix[i][u] == this.matrix[this.shape.bottomCoordinate][u]) {
+          this.addFigureToMatrix(this.shape.bottomCoordinate);
+          break;
+        }
+      }
+    }
+
+    this.shape.bottomCoordinate++;
+
+
+  }
+  addFigureToMatrix(topLine) {
+    for (let i = 0; i < 4; i++) {
+      this.matrix[topLine - 4 + i] = this.shape.matrix[i];
+    }
+    this.shape = new IShape();
   }
 
   // nextFigure() { //maybe I do not need this function if I add bottom figure and renderField will write field
@@ -28,22 +47,27 @@ class Game {
   // }
   newMatrix() {
     let matrix = [];
-    for (let i = 0; i < 23; i++) {
+    for (let i = 0; i < 24; i++) {
       let line = [];
       for (let u = 0; u < 10; u++) {
-        line.push(0);
+        if (i == 23) {
+          line.push(1);
+        } else {
+          line.push(0);
+        }
+
       }
       matrix.push(line);
     }
     return matrix;
   }
   renderField() {
-    for (let i = 0; i < 23; i++) {
+    for (let i = 0; i < 24; i++) {
       for (let u = 0; u < 10; u++) {
         this.field.fieldBlocks[i * 10 + u].innerText = 0;
       }
     }
-    for (let i = 0; i < 23; i++) {
+    for (let i = 0; i < 24; i++) {
       for (let u = 0; u < 10; u++) {
         if (i > this.shape.bottomCoordinate - 5 && i < this.shape.bottomCoordinate) {
           this.field.fieldBlocks[i * 10 + u].innerText = this.shape.matrix[i - this.shape.bottomCoordinate + 4][u];
@@ -60,7 +84,7 @@ class Field {
     this.gameField = document.createElement("div");
     this.gameField.id = "field";
     document.body.append(this.gameField);
-    for (let i = 0; i < 23; i++) {
+    for (let i = 0; i < 24; i++) {
       let line = document.createElement("div");
       line.className = "line";
       for (let u = 0; u < 10; u++) {
@@ -122,7 +146,7 @@ class IShape extends Tetromino {
         [0, 0, 0, -1, 0, 0, -1, 0, 0, 0],
         [0, 0, 0, -1, 0, 0, -1, 0, 0, 0],
         [0, 0, 0, -1, 0, 0, -1, 0, 0, 0],
-        [0, 0, 0, 1, 1, 1, 1, 0, 0, 0]
+        [0, 0, -0, 1, 1, 1, 1, 0, 0, 0]
       ]
     ];
     this.positionNumber = Math.round(Math.random() * (this.positions.length - 1));
